0cd3555e66f3f4def2711f9c947a9aa78592e649
Piotroszko <piotroszko2@gmail.com>
2021-11-22 14:19:51 +0100

    MenuIcon Manager dodany

---
+++ Assets/_scripts/UI/MenuIconManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuIconManager : MonoBehaviour
+{
+  public GameObject menuCanvas;
+  public GameObject inventoryView;
+  public GameObject iventoryUpgrade;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+  public void ChangeView(string selectedView)
+  {
+    if (selectedView == "back")
+    {
+      this.menuCanvas.SetActive(false);
+    }
+    else if (selectedView == "inventory")
+    {
+      this.iventoryUpgrade.SetActive(false);
+      this.inventoryView.SetActive(true);
+    }
+    else if (selectedView == "upgrade")
+    {
+      this.iventoryUpgrade.SetActive(true);
+      this.inventoryView.SetActive(false);
+    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
