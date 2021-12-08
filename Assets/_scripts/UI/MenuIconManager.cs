080ea708a0b84d6c2ad08750497d39879b6f1c12
Piotroszko <piotroszko2@gmail.com>
2021-12-08 12:05:26 +0100

    Views added, basic tree view added, tree upgrades script added

--- Assets/_scripts/UI/MenuIconManager.cs
+++ Assets/_scripts/UI/MenuIconManager.cs
@@ -8,6 +8,10 @@ public class MenuIconManager : MonoBehaviour
   public GameObject inventoryView;
   public GameObject iventoryUpgrade;
   public GameObject InGameMenuCanvas;
+
+  public GameObject characterView;
+  public GameObject treeView;
+  public GameObject settingsView;
   // Start is called before the first frame update
   void Start()
   {
@@ -22,16 +26,38 @@ public class MenuIconManager : MonoBehaviour
     }
     else if (selectedView == "inventory")
     {
-      this.iventoryUpgrade.SetActive(false);
+      this.disableView();
       this.inventoryView.SetActive(true);
     }
     else if (selectedView == "upgrade")
     {
+      this.disableView();
       this.iventoryUpgrade.SetActive(true);
-      this.inventoryView.SetActive(false);
+    }
+    else if (selectedView == "character")
+    {
+      this.disableView();
+      this.characterView.SetActive(true);
+    }
+    else if (selectedView == "tree")
+    {
+      this.disableView();
+      this.treeView.SetActive(true);
+    }
+    else if (selectedView == "settings")
+    {
+      this.disableView();
+      this.settingsView.SetActive(true);
     }
   }
-
+  void disableView()
+  {
+    if (this.iventoryUpgrade) this.iventoryUpgrade.SetActive(false);
+    if (this.inventoryView) this.inventoryView.SetActive(false);
+    if (this.characterView) this.characterView.SetActive(false);
+    if (this.treeView) this.treeView.SetActive(false);
+    if (this.settingsView) this.settingsView.SetActive(false);
+  }
   // Update is called once per frame
   void Update()
   {
