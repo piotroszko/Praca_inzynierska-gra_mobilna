9def9c781ad411ee0b5514fd9553b3465d36b1c3
Piotroszko <piotroszko2@gmail.com>
2021-12-10 14:50:46 +0100

    Scripts setup for skill tree

--- Assets/_scripts/Data/treeUpgrades.cs
+++ Assets/_scripts/Data/treeUpgrades.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class treeUpgrades : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  [Serializable]
+  private class TreeNode
+  {
+    public int ID;
+    public int row;
+    public int pos;
+    public string nodeName;
+    public string desc;
+  }
+  [SerializeField] private TreeNode[] nodes;
+  public List<int> nodesOwned = new List<int>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
 }
