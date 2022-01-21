138d54da8eae38f2f475e1ae8c1708fd1416e73a
Piotroszko <piotroszko2@gmail.com>
2022-01-21 14:53:29 +0100

    Calculate damage recived with inventory reduction

--- Assets/_scripts/Combat/PlayerCombatController.cs
+++ Assets/_scripts/Combat/PlayerCombatController.cs
@@ -9,9 +9,35 @@ public class PlayerCombatController : MonoBehaviour
 
   public float speed = 1;
   float lastThrowDate;
+
+  private GameObject pm;
   void Start()
   {
     lastThrowDate = Time.time;
+    this.pm = GameObject.FindWithTag("PlayerManager").gameObject;
+  }
+  public void DamagePlayer(int value)
+  {
+    float damage = value;
+    if (pm.GetComponent<treeUpgrades>().nodesOwned.Exists(x => x == 2))
+      damage = value * 0.8f;
+    damage = (100f / (100f + pm.GetComponent<CharacterValues>().pointsDefense)) * damage;
+    Inventory inv = pm.GetComponent<Inventory>();
+    float def1 = 0f;
+    if (inv.equippedCollar != null)
+    {
+      float defTmp1 = inv.equippedCollar.defense + (inv.equippedCollar.upgradeInfo.sumTypeItem(true) * inv.equippedCollar.defense);
+      def1 = defTmp1 + (inv.equippedCollar.upgradeInfo.sumTypeMoney(true) * defTmp1);
+    }
+    float def2 = 0f;
+    if (inv.equippedCoat != null)
+    {
+      float defTmp2 = inv.equippedCoat.defense + (inv.equippedCoat.upgradeInfo.sumTypeItem(true) * inv.equippedCoat.defense);
+      def2 = defTmp2 + (inv.equippedCoat.upgradeInfo.sumTypeMoney(true) * defTmp2);
+    }
+    damage = (100f / (100f + def1 + def2)) * damage;
+
+    gameObject.GetComponent<Health>().Damage(damage);
   }
 
   void Update()
