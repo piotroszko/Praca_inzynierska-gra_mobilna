ce1cb4839497a811832a0c03f4b144c059cb3031
Piotroszko <piotroszko2@gmail.com>
2022-02-01 15:36:06 +0100

    Flower boss added

---
+++ Assets/_scripts/Combat/FlowerBossManager.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random=System.Random;
+
+public class FlowerBossManager : MonoBehaviour
+{
+    [SerializeField] List<float> attackYValues = new List<float>();
+    public GameObject flowerHead;
+    public GameObject projectilePrefab;
+    public Sprite projectileSprite;
+    public GameObject shotPoint;
+    public float timeForSection = 6.0F;
+
+    public float attackSpeed = 1.9F;
+    public bool isMoving = false;
+    float headMoveSpeed = 2.0F;
+
+    private float startTime;
+    private float sectionStartTime;
+    private float journeyLength;
+    private Vector2 startPosition;
+    private float lastShotTime;
+    private Animator anim;
+    int indexToMoveTo = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackYValues.Sort();
+        this.anim = flowerHead.GetComponent<Animator>();
+    }
+    void OnEnable() {
+        flowerHead.transform.position = new Vector2(flowerHead.transform.position.x, attackYValues[1]);
+        startTime = Time.time;
+        lastShotTime = Time.time;
+
+        startPosition = flowerHead.transform.position;
+        journeyLength = Vector2.Distance(flowerHead.transform.position, new Vector2(flowerHead.transform.position.x, attackYValues[0]));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeSection();
+        this.MoveToIndex();
+        if(flowerHead.transform.position.y == attackYValues[indexToMoveTo]) {
+            isMoving = false;
+        } else {
+            isMoving = true;
+        }
+        Shot();
+    }
+    void ChangeSection() {
+        if(Time.time - sectionStartTime > timeForSection) {
+            startTime = Time.time;
+            sectionStartTime = Time.time;
+            startPosition = flowerHead.transform.position;
+            ChangePostionIndex();
+            journeyLength = Vector2.Distance(flowerHead.transform.position, new Vector2(flowerHead.transform.position.x, attackYValues[indexToMoveTo]));
+        }
+    }
+    void ChangePostionIndex () {
+        Random rnd = new Random();
+        if(attackYValues.Count > indexToMoveTo + 1 && indexToMoveTo - 1 >= 0) {
+            int random = rnd.Next(1, 3);
+            if(random == 1) {
+                indexToMoveTo++;
+            } else {
+                indexToMoveTo--;
+            }
+        } else if (attackYValues.Count > indexToMoveTo + 1 && indexToMoveTo - 1 < 0) {
+            indexToMoveTo++;
+        } else if (attackYValues.Count == indexToMoveTo + 1 && indexToMoveTo - 1 >= 0)
+        {
+            indexToMoveTo--;
+        }
+    }
+    void Shot(){
+        if(Time.time - lastShotTime > attackSpeed && !isMoving) {
+            this.anim.Play("Attack");
+            lastShotTime = Time.time;
+            Invoke("ShotProjectile", 0.15F);
+        }
+    }
+    void ShotProjectile() {
+        GameObject projectile = Instantiate(projectilePrefab, shotPoint.transform.position, shotPoint.transform.rotation);
+        projectile.GetComponent<EnemyProjectileManager>().Setup(projectileSprite, 10f);
+    }
+    void MoveToIndex() {
+        float distCovered = (Time.time - startTime) * headMoveSpeed;
+
+        float fracJourney = distCovered / journeyLength;
+
+        flowerHead.transform.position = Vector3.Lerp(startPosition, new Vector2(flowerHead.transform.position.x, attackYValues[indexToMoveTo]), fracJourney);
+    }
+}
